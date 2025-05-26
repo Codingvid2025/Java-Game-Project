af943bf022a5768ae8772ddc0cfe519bb92458b5
Vidyuth <svidyuth200@gmail.com>
2025-05-26 13:22:23 -0400

    Delete demo directory

--- demo/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module random.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens random.demo to javafx.fxml;
-    exports random.demo;
-}
